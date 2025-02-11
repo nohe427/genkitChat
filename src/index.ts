9dd04513e4590f4fc8a22fb77c4e38023c0e9b67
Alexander Nohe <nohe@google.com>
2025-02-10 19:49:55 -0800

    shove chat sessions in a bucket

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ const storage = new Storage({ projectId: projectId })
 class GcsSessionStorage<S = any> implements SessionStore<S> {
   async get(sessionId: string): Promise<SessionData<S> | undefined> {
     try {
-      const file = await storage.bucket(bucket).file(sessionId).download();
+      const file = await storage.bucket(bucket).file(`chats/${sessionId}`).download();
       return JSON.parse(file[0].toString("utf-8"));
     } catch (error) {
       console.error(error);
@@ -43,7 +43,7 @@ class GcsSessionStorage<S = any> implements SessionStore<S> {
 
   async save(sessionId: string, data: Omit<SessionData<S>, 'id'>): Promise<void> {
     try {
-      await storage.bucket(bucket).file(sessionId).save(JSON.stringify(data));
+      await storage.bucket(bucket).file(`chats/${sessionId}`).save(JSON.stringify(data));
       console.log('file written to GCS');
     }
     catch (error) {
